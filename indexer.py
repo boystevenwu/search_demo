d269801399ef63f107918f3a5b591fa9398351cd
Steven Wu <swimacbook@SwiMacBook.local>
2022-03-03 22:59:33 -0800

    refine code

--- indexer.py
+++ indexer.py
@@ -20,33 +20,37 @@ def tokenize(file_text):
     return listt
 
 
-def build_index(ls, url):
+def build_index(tokens, url):
     global freq
 
     # stop_words list that will be counted with lower weights
-    op = open('./stopword.txt', "r", encoding="utf-8")
-    stop_words = set()
-    for a in op:
-        lineplus = re.sub(r'[^A-Za-z0-9]+', ' ', a)
-        for item in lineplus.lower().split():
-            stop_words.add(ps.stem(item))
+    with open('./stopwords.txt', "r", encoding="utf-8") as f:
+        stop_words = set()
+        for line in f:
+            lineplus = re.sub(r'[^A-Za-z0-9]+', ' ', line)
+            for item in lineplus.lower().split():
+                stop_words.add(ps.stem(item))
+
+    # # ignore stopwords if they are over 2/3 of the sentence
+    # if len(stop_words) / len(tokens) > (2 / 3):
+    #     stop_words = set()
 
     # build a dictionary for all the words with respective tf
-    for word in ls:
+    for word in tokens:
         if word not in freq.keys():
             freq[word] = dict()
 
         # if the word in the query is a stop word, then we will regard it as 1/20 regular word.
         if word in stop_words:
             if url in freq[word].keys():
-                freq[word][url] += 0.05 / len(ls)
+                freq[word][url] += 0.05 / len(tokens)
             else:
-                freq[word][url] = 0.05 / len(ls)
+                freq[word][url] = 0.05 / len(tokens)
         else:
             if url in freq[word].keys():
-                freq[word][url] += 1 / len(ls)
+                freq[word][url] += 1 / len(tokens)
             else:
-                freq[word][url] = 1 / len(ls)
+                freq[word][url] = 1 / len(tokens)
 
 
 def calculate_tf_idf(tf):
